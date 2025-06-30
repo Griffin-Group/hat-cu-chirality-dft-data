6fe153027882d97064f7b9475d23760fda313734
Bernard Field <51108483+bfield1@users.noreply.github.com>
2025-06-30 15:45:44 -0700

    Tar large files

---
+++ tar_large_files.sh
@@ -0,0 +1,2 @@
+tar --create --file hat-cu-chirality-dft-data-large-files.tar */*/trajectory_OUTCARs.zip */*/CHGCAR.zip */*/PROCAR.gz */*/PARCHG_?.gz HAT-Cu_Y/2d.parchg/PARCHG_1_raw.gz */*/PARCHG_??.gz HAT-Cu-AgSlab/3?.*/vasprun.xml.gz HAT-Cu-AgSlab5/2?.*/vasprun.xml.gz
+# To un-tar, simply `tar -x -f hat-cu-chirality-dft-data-large-files.tar`. The directory structure was preserved.
